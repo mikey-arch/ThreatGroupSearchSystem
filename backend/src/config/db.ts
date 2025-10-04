0053f659b53a493b1a8f07507ddeec7de635399b
lucasziiN <lucassoda2@gmail.com>
2025-10-05 02:23:57 +1300

    Added more information and fields to current data doc. Ensured information is accurate too.

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,7 +5,7 @@ export const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MONGODB CONNECTED SUCCESSFULLY!");
     } catch (error) {
-        console.error("Error conencting to MONGODB", error);
+        console.error("Error connecting to MONGODB", error);
         process.exit(1);
     }
 }
