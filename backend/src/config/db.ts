79651b924d4f9dca0bb90d9397142afae290f694
mikey-arch <workwithmikey@outlook.com>
2025-10-06 21:45:48 +1300

    revert some changes

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,9 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-        dbName: "test" //db name
-        });
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MONGODB CONNECTED SUCCESSFULLY!");
     } catch (error) {
         console.error("Error connecting to MONGODB", error);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-// import {Route, Routes} from "react-router";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
